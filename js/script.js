bd60415823e9c75a610392852a27ec61d1cf0233
giuse <giuseppedefilippo03@gmail.com>
2024-11-11 16:48:33 +0100

    added js clock

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+"use strict"
+console.clear()
+
 /**Descrizione:*
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce. (Per favorire il lavoro tutor mettete inizialmente un timer di 5-10 sec e non 30)
@@ -7,27 +10,36 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 //Math.floor(Math.random() * 99);
 const numbertoguess = [];
-const numerorandom = document.getElementById('numbers-list').innerHTML;
-const count = document.getElementById('countdown');
-let seconds = 6;
-let interval;
+const numbersList = document.getElementById('numbers-list');
+const countDown = document.getElementById('countdown');
+const answersForm = document.getElementById('answers-form');
 
-function countdown(){
-    seconds= 6;
-    count.innerText = seconds;
-    
-    Interval = setInterval(function() {
-        seconds--;
-        count.innerText = seconds; // Aggiorna il display
+
+
+//function countdown(){
+
+    let seconds = 6;
+
+    let tempo = setInterval( function () {
+        
+        // count.innerText = seconds; // Aggiorna il display
 
         // Quando il tempo arriva a zero, ferma il timer
-        if (seconds <= 0) {
-            clearInterval(Interval);
-            count.innerText = "Tempo scaduto!";
+        if (seconds > 0) {
+            seconds--;
+            countDown.innerText = seconds;
+        
+            
+        } else{
+            countDown.remove();
+            numbersList.remove();
+            answersForm.classList.remove('d-none');
+            ;
         }
     }, 1000);
-}
-countdown();
+    
+//}
+//countdown();
 
 function random(){
     numbertoguess.length = 0;
@@ -36,7 +48,14 @@ function random(){
         numbertoguess.push(numero);
         
     }
-    document.getElementById('numbers-list').innerHTML = numbertoguess.join(", ");
+    numbersList.innerHTML = numbertoguess.join(", ");
 }
 random();
  
+// bottone
+
+const bottone = document.querySelector('button.btn');
+bottone.addEventListener('click' , function(event){
+    event.preventDefault()
+
+})
